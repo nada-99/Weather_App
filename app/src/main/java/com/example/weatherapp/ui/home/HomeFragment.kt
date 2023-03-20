a7f2a93a8fb123f7a5cba5b1611a7c5b1a6bbf99
Nada Ayman <nadaayman9997@gmail.com>
2023-03-20 04:59:03 +0200

    first version of ui

--- app/src/main/java/com/example/weatherapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/home/HomeFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weatherapp.R
+import com.example.weatherapp.R.layout.fragment_home
 
 
 class HomeFragment : Fragment() {
@@ -20,6 +20,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(fragment_home, container, false)
     }
 }
