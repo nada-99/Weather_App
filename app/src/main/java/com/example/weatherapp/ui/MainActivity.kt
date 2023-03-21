7c075f15f9e8f8919014f9c1e5006c106cf85a69
Nada Ayman <nadaayman9997@gmail.com>
2023-03-21 12:42:24 +0200

    design home , fav and alerts fragments

--- app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.weatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.weatherapp.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigationView = findViewById(R.id.navigation_view)
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        val actionBar = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.menu)
+        actionBar!!.setDisplayShowHomeEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+        navController = findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(navigationView, navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
